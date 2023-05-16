5d5a91c47a7b50ed86933192ed227823c3a0a087
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2023-05-17 00:12:07 +0300

    docker

---
+++ user/data/02_init_contact.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO public.contact(
+	email, phone)
+	VALUES ('email1@gmail.com', '79998887760');
+
+
+INSERT INTO public.contact(
+	email, phone)
+	VALUES ('email2@gmail.com', '79998887761');
+
+
+INSERT INTO public.contact(
+	email, phone)
+	VALUES ('email3@gmail.com', '79998887762');
+
+
+INSERT INTO public.contact(
+	email, phone)
+	VALUES ('email4@gmail.com', '79998887763');
+
+
+INSERT INTO public.contact(
+	email, phone)
+	VALUES ('email5@gmail.com', '79998887764');
---
+++ user/data/03_init_credential.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('q', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'USER');
+
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('qw', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'USER');
+
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('qwe', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'USER');
+
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('qwer', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'USER');
+
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('qwert', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'USER');
+
+
+
+INSERT INTO public.credential(
+	username, password, role)
+	VALUES ('admin', '$2a$10$6kKh.2rEZgjSXyES6S4iIO6OtrYBSycWswnX6X0VZGmDWfsLazG0W', 'ADMIN');
